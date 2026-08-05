140140e48a462059e2203283dc6c0142a13743b9
Your Name <your-email@example.com>
2026-08-05 14:28:01 +0530

    orderSlice implemented

--- src/store/orderSlice.js
+++ src/store/orderSlice.js
@@ -6,12 +6,40 @@ const initialState = {
   loading: false,
   error: null,
 };
+export const placeOrder = createAsyncThunk(
+  "order/placeOrder",
+  async (order) => {
+    const userId = localStorage.getItem("userId");
+
+    const response = await fetch(
+      `${DATABASE_URL}/orders/${userId}.json`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(order),
+      }
+    );
+
+    const data = await response.json();
+
+    return {
+      id: data.name,
+      ...order,
+    };
+  }
+);
 
 const orderSlice = createSlice({
   name: "order",
   initialState,
   reducers: {},
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder.addCase(placeOrder.fulfilled, (state, action) => {
+  state.orders.push(action.payload);
+});
+  },
 });
 
 export default orderSlice.reducer;
